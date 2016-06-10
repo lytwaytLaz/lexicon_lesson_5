239539c3a6bd793ed46a3a506a8af6b495a61534
lytwaytLaz <laszlo.hago@gmail.com>
2016-06-10 15:55:43 +0200

    Nothing really

--- lesson_5_3/Main.java
+++ lesson_5_3/Main.java
@@ -1,9 +1,14 @@
 package lesson_5_3;
 
+import java.util.Collection;
+
 /**
  * @author László Hágó
  * @version 1.0
  * @since 2016-06-10
  */
 public class Main {
+    public static void main(String[] args) {
+        Collection<?>
+    }
 }
---
+++ lesson_5_4.java
@@ -0,0 +1,8 @@
+/**
+ * @author László Hágó
+ * @version 1.0
+ * @since 2016-06-10
+ */
+public class lesson_5_4 {
+    
+}
